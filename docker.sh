680e5e3baa2ab92430f0280f87a67988b2aa9a37
jgounand <joris@gounand.fr>
2019-11-08 16:38:43 +0100

    docker

--- docker.sh
+++ docker.sh
@@ -2,3 +2,4 @@ mkdir /goinfre/${USER}/ 2> /dev/null
 mkdir /goinfre/${USER}/docker 2> /dev/null
 ln -s /goinfre/${USER}/docker ~/Library/Containers/com.docker.docker
 docker build -t debian .
+docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -ti -v ~/:/root debian
